b064371e085b03ed723fb63784ef66fc3f49beb2
AlexJHZheng <autumatic@gmail.com>
2024-03-18 09:48:57 -0300

    adicionar verifyToken api

--- src/api/user.js
+++ src/api/user.js
@@ -1,12 +1,30 @@
 // 使用axios进行接口调用
 import request from "@/utils/request";
+import { getCookie } from "@/utils/cookie";
+
+// cookie中取出token
+// const token = getCookie("token");
+// console.log(token, "token");
+// const headers = {
+//   Authorization: `Bearer ${token}`,
+// };
 
 //定义注册方法，传入用户密码对象
-
-export function LoginApiTest(data) {
+//登陆接口
+export function LoginApi(data) {
   return request({
     url: "/userlogin",
     method: "post",
     data,
+  });
+}
+//验证token接口
+export function VerifyToken() {
+  return request({
+    url: "/verifyToken",
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${getCookie("token")}`,
+    },
   });
 }
